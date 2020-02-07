21bac6fa93c4496effe07d6ca82065687bb25ff1
Giovanny <giovynet@icloud.com>
2020-02-06 23:22:27 -0500

    Cambio de nombre de paquetes

--- src/main/java/com/example/drools/AppStatefull.java
+++ src/main/java/com/example/drools/AppStatefull.java
@@ -1,12 +1,10 @@
 package com.example.drools;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.kie.api.KieServices;
 import org.kie.api.runtime.KieContainer;
-import org.kie.api.runtime.StatelessKieSession;
+import org.kie.api.runtime.KieSession;
 
 import com.example.drools.model.Person;
 
@@ -16,16 +14,14 @@ public class AppStatefull {
 		// 1) Bootstrapping a Rule Engine Session
 		KieServices ks = KieServices.Factory.get();
 		KieContainer kContainer = ks.getKieClasspathContainer();
-		StatelessKieSession kieSession = kContainer.newStatelessKieSession("ksession-stateless");
+		KieSession kSession = kContainer.newKieSession("ksession-statefull-opportunity");
 
-		List<Person> lstPerson = new ArrayList<>();
-		lstPerson.add(new Person(1L, "Alonso Quijano", new BigDecimal(3000.00), new BigDecimal(3000.00), true));
-		lstPerson.add(new Person(2L, "Dulcinea del Toboso", new BigDecimal(5000.00), new BigDecimal(4750.00), true));
-		lstPerson.add(new Person(1L, "Sancho Panza", new BigDecimal(1000.00), new BigDecimal(7900.00), false));
-
-		kieSession.execute(lstPerson);
-		
-		lstPerson.forEach(System.out::println);
+		Person person = new Person(1L, "Alonso Quijano", new BigDecimal(3000.00), new BigDecimal(3000.00), true);
+		kSession.insert(person);
+		int fired = kSession.fireAllRules();
+		System.out.println("Number of Rules executed = " + fired);
+		System.out.println("Status: " + person.getStatus());
+		kSession.dispose();
 
 	}
 
--- src/main/java/com/example/drools/AppStateless.java
+++ src/main/java/com/example/drools/AppStateless.java
@@ -1,10 +1,12 @@
 package com.example.drools;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.kie.api.KieServices;
 import org.kie.api.runtime.KieContainer;
-import org.kie.api.runtime.KieSession;
+import org.kie.api.runtime.StatelessKieSession;
 
 import com.example.drools.model.Person;
 
@@ -14,13 +16,16 @@ public class AppStateless {
 		// 1) Bootstrapping a Rule Engine Session
 		KieServices ks = KieServices.Factory.get();
 		KieContainer kContainer = ks.getKieClasspathContainer();
-		KieSession kSession = kContainer.newKieSession("ksession-statefull");
+		StatelessKieSession kieSession = kContainer.newStatelessKieSession("ksession-stateless-opportunity");
 
-		Person person = new Person(1L, "Alonso Quijano", new BigDecimal(3000.00), new BigDecimal(3000.00), true);
-		kSession.insert(person);
-		int fired = kSession.fireAllRules();
-		System.out.println("Number of Rules executed = " + fired);
-		System.out.println("Status: " + person.getStatus());
+		List<Person> lstPerson = new ArrayList<>();
+		lstPerson.add(new Person(1L, "Alonso Quijano", new BigDecimal(3000.00), new BigDecimal(3000.00), true));
+		lstPerson.add(new Person(2L, "Dulcinea del Toboso", new BigDecimal(5000.00), new BigDecimal(3750.00), true));
+		lstPerson.add(new Person(1L, "Sancho Panza", new BigDecimal(1000.00), new BigDecimal(7900.00), false));
+
+		kieSession.execute(lstPerson);
+		
+		lstPerson.forEach(System.out::println);
 
 	}
 
